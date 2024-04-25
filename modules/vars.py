7d770412ffc3d535a6c2efbc7c453b4e66f09ea1
hectorog6612 <159784158+hectorog6612@users.noreply.github.com>
2024-04-25 09:27:00 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = "22257490"
-api_hash = "0f033d8a410fbf8157d0d4ba614f6ce4"
-bot_token = "7044634855:AAF0sqeN6HwHasq9mijlqcRnVGmfMsjCpA4"
+api_id = "22308019"
+api_hash = "fe49bff9eaadccecb58ded07aff33e88"
+bot_token = "7152255228:AAGRJc1utg-XkhLfkhbGfGqLsxNUKbxvyw0"
